66ed66500bcfd40975bafd9c13505f2de959d387
Steven Purves <StevenPurves@outlook.com>
2019-10-12 16:30:23 +0100

    files added

---
+++ Assets/Scripts/PortalTextureSetup.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PortalTextureSetup : MonoBehaviour
+{
+
+    public Camera pinkCam;
+    public Camera blueCam;
+
+    public Material pinkMat;
+    public Material blueMat;
+
+
+    void Start()
+    {
+        if (blueCam.targetTexture != null)
+        {
+            blueCam.targetTexture.Release();
+        }
+        blueCam.targetTexture = new RenderTexture(Screen.width, Screen.height, 24);
+        blueMat.mainTexture = blueCam.targetTexture;
+
+
+        if(pinkCam.targetTexture != null){
+            pinkCam.targetTexture.Release();
+        }
+        pinkCam.targetTexture = new RenderTexture(Screen.width,Screen.height,24);
+        pinkMat.mainTexture = pinkCam.targetTexture;
+    }
+
+}
---
+++ Assets/Scripts/Teleport.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Teleport : MonoBehaviour {
+
+
+    public Transform player;
+    public Transform reciever;
+
+   
+
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.tag == "Player")
+        {
+            
+            float dot = Vector3.Dot(transform.forward, (player.position - transform.position).normalized);
+
+
+            if (dot < 0f)
+            {
+                float rotationDiff = -Quaternion.Angle(transform.rotation, reciever.rotation);
+                rotationDiff += 180;
+                player.Rotate(Vector3.up, rotationDiff);
+
+                Vector3 portalToPlayer = player.position - transform.position;
+
+                Vector3 positionOffset = Quaternion.Euler(0f, rotationDiff, 0f) * portalToPlayer;
+                player.position = reciever.position + positionOffset;
+            
+            }
+        }
+    }
+
+}
